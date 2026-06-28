f109a1895df67a0d96be29720f72841c837f5b66
Cycl0o0 <contact@cyclooo.fr>
2026-06-28 14:46:18 +0200

    fix(android): missing layout.padding import in QueueScreen

--- gui/android/app/src/main/java/fr/cyclooo/opendeezer/ui/screens/QueueScreen.kt
+++ gui/android/app/src/main/java/fr/cyclooo/opendeezer/ui/screens/QueueScreen.kt
@@ -3,6 +3,7 @@ package fr.cyclooo.opendeezer.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
